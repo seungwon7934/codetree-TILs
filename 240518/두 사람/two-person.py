badd33cecb4b55a33d2fef6d6d77f0a5dc18d6fd
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-18 19:17:21 +0900

    Title: 두 사람, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/두 사람/two-person.py
+++ 240518/두 사람/two-person.py
@@ -7,4 +7,4 @@ sex1 = arr1[1]
 age2 = int(arr2[0])
 sex2 = arr2[1]
 
-print(1) if (age1 >= 1 and sex1 == 'M') or (age2 >= 1 and sex2 == 'M') else print(0)
+print(1) if (age1 >= 19 and sex1 == 'M') or (age2 >= 19 and sex2 == 'M') else print(0)
