1fbf81c3cf4ebbc0ac3186946e930a0bec5337b1
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-18 19:20:48 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240518/윤년인가/is-leap-year.py
+++ 240518/윤년인가/is-leap-year.py
@@ -1,9 +1,9 @@
 y = int(input())
 
 if(y % 100 == 0 and y % 400 != 0):
-    print("false")
+    print(false)
 else:
     if(y % 4 == 0):
-        print("true")
+        print(true)
     else:
-        print("false")
+        print(false)
