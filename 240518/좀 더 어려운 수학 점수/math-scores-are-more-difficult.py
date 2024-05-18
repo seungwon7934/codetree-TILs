8bcf9d102b9e910949b917acefdda41d4bd3d745
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-18 19:11:58 +0900

    Title: 좀 더 어려운 수학 점수, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240518/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,14 @@
+a1, a2 = map(int, input().split())
+b1, b2 = map(int, input().split())
+
+
+if(a1 == b1):
+    if(a2 > b2):
+        print("A")
+    else:
+        print("B")
+else:
+    if a1 > b1:
+        print("A")
+    else:
+        print("B")
