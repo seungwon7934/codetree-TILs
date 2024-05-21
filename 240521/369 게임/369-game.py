faa1aa26d39e78d9c6911ecb9e25617c6258e2d7
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-21 22:16:08 +0900

    Title: 369 게임, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240521/369 게임/369-game.py
+++ 240521/369 게임/369-game.py
@@ -1,17 +1,23 @@
 n = int(input())
 
-for i in range(1, n+1):
-    if(i == 100):
-        print(i)
-    elif(i >= 10):
-        a = int(i/10);
-        b = i % 10;
-        if(a % 3 == 0 or b == 3 or b == 6 or b == 9 or i % 3 == 0):
-            print(0, end=' ')
-        else:
+# 1300은 틀림
+
+for  i in range(1, n+1):
+    if(i <= n // 100):
+        if(i % 3 == 0):
+            print(0, end=' ')     
+        else:     
             print(i, end=' ')
-    else:
+    while(i > n // 100):
+        q = i // 10
+        r = i % 10
+
         if(i % 3 == 0):
             print(0, end=' ')
+        elif(q == 3 or q == 6 or q == 9):
+            print(0, end=' ')
+        elif(r == 3 or r == 6 or r == 9):
+            print(0, end=' ')
         else:
             print(i, end=' ')
+        i = i // 100
