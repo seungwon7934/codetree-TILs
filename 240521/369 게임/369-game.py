c85125129d0e74db61e9eaa02797209a77f74478
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-21 21:55:54 +0900

    Title: 369 게임, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240521/369 게임/369-game.py
+++ 240521/369 게임/369-game.py
@@ -4,9 +4,10 @@ for i in range(1, n+1):
     if(i == 100):
         print(i)
     elif(i >= 10):
-        a = i/10;
+        a = int(i/10);
         b = i%10;
-        if(a == 3 or b == 3):
+        # print(a, b)
+        if(a == 3 or b == 3 or i % 3 == 0):
             print(0, end=' ')
         else:
             print(i, end=' ')
