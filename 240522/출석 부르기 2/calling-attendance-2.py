20d66c93a2b95fb9812a08487dcfc3c58cc0e895
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-22 19:56:28 +0900

    Title: 출석 부르기 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/출석 부르기 2/calling-attendance-2.py
+++ 240522/출석 부르기 2/calling-attendance-2.py
@@ -1,7 +1,16 @@
+n = 0
+
 while(1):
     n = int(input())
-    if(1 <= n <= 100):
-        print(n)
+    if(1 <= n <= 4):
+        if(n == 1):
+            print("John")
+        elif(n == 2):
+            print("Tom")
+        elif(n == 3):
+            print("Paul")
+        else:
+            print("Sam")
     else:
         print("Vacancy")
         break
