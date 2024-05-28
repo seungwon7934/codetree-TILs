ce4d303d4f30ae7ca4710116478605ca46fd5cf2
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-05-28 09:54:23 +0900

    Title: 3n + 1 수열, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240528/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,13 @@
+N = int(input())
+cnt = 0
+while(N != 1):
+    if(N % 2 == 0):
+        N /= 2
+        cnt += 1
+        continue
+    if(N % 2 != 0):
+        N = N * 3 + 1
+        cnt += 1
+        continue
+
+print(cnt)
