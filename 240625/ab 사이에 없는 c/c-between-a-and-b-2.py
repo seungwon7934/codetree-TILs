ca318151e31d65014078d6a31081598aff09c51c
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-25 15:38:53 +0900

    Title: ab 사이에 없는 c, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240625/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,17 @@
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+tf = False
+
+for i in range(a, b + 1):
+    if(i % c == 0):
+        tf = True
+        break
+
+if (tf == True):
+    print("NO")
+else:
+    print("YES")
