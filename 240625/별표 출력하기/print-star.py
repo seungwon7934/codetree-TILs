e100d291f4048af3ccbeb3b506b9c09ee8e7b938
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-25 16:05:15 +0900

    Title: 별표 출력하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/별표 출력하기/print-star.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n * 2):
+    if(i <= n):
+        for j in range(i):
+            print("* ", end='')
+        print()
+    else:
+        for j in range(i - n, 5):
+            print("* ", end='')
+        print()
