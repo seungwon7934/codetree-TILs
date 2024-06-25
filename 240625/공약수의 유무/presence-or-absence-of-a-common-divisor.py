40c53981c44d9f055548ad80f301a5ddee6dafd9
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-25 15:36:17 +0900

    Title: 공약수의 유무, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+tf = False
+
+for i in range(a, b+1):
+    if(1920 % i == 0 or 2880 % i == 0):
+        tf = True
+        break
+
+if (tf == True):
+    print(1)
+else:
+    print(0)
