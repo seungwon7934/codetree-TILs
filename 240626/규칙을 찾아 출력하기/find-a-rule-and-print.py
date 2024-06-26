402837e99c71d3900ab33396d5c8dc5dce99af1f
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-26 12:39:59 +0900

    Title: 규칙을 찾아 출력하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240626/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    if(i == 0 or i == n - 1):
+        for _ in range(n):
+            print("* ",end='')
+        print()
+    else:
+        for j in range(n):
+            if(j < i or j == n - 1):
+                print("* ",end='')
+            else:
+                print("  ", end='')
+        print()
