7d95c74b5688a4aacc4720c1089996d7bfcdaab8
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-27 15:40:36 +0900

    Title: 알파벳 출력하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/알파벳 출력하기/print-alphabet.py
+++ 240627/알파벳 출력하기/print-alphabet.py
@@ -1,9 +1,9 @@
 n = int(input())
 
-aph = 65
+aph = 0
 
 for i in range(n):
-    for j in range(i+ 1):
-        print(chr(aph),end='')
+    for j in range(i + 1):
+        print(chr(aph % 26 + 65),end='')
         aph += 1
     print()
