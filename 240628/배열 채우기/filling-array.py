7d47e6dae4cdfdbdf53828593e55a03abb395f82
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-06-28 11:01:49 +0900

    Title: 배열 채우기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/배열 채우기/filling-array.py
+++ 240628/배열 채우기/filling-array.py
@@ -5,6 +5,8 @@ r_arr = list()
 for i in range(len(arr)):
     if(arr[i] != 0):
         r_arr.append(arr[i])
+    else:
+        break
 
 for i in range(len(r_arr)):
     print(r_arr[len(r_arr) - 1 - i], end=' ')
