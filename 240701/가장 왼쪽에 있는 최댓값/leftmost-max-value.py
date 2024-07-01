50f440768b8bfc46e6a9476ee64832225c501ed2
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-01 11:29:02 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240701/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -7,14 +7,14 @@ for i in range(n):
     max_num = arr[0]
     max_left_idx = 1
 
-    for j in range(start_idx - 1):
+    for j in range(start_idx):
         
         if(max_num < arr[j]):
             max_num = arr[j]
             max_left_idx = j + 1
 
     print(max_left_idx, end=' ')
-    start_idx = max_left_idx
+    start_idx = max_left_idx - 1
 
     if(max_left_idx == 1):
         break
