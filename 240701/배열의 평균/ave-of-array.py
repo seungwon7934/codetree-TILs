2eab172f96c0039cc67cc09c7cc07a29fae232c0
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-01 13:11:07 +0900

    Title: 배열의 평균, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240701/배열의 평균/ave-of-array.py
@@ -0,0 +1,22 @@
+arr2 = []
+
+for _ in range(2):
+    arr1 = list(map(int, input().split()))
+    arr2.append(arr1)
+
+
+for i in range(2):
+    print(sum(arr2[i]) / len(arr2[i]), end=' ')
+print()
+
+total = 0
+
+for i in range(4):
+    col_sum = 0
+    for j in range(2):
+        total += arr2[j][i]
+        col_sum += arr2[j][i]
+    print(col_sum / len(arr2), end=' ')
+print()
+
+print(total / (len(arr2[0]) * len(arr2)))
