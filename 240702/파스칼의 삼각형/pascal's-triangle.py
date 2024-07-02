406fba06d8721c0a4f51085c4f8cffa117d5b7c2
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-02 09:45:38 +0900

    Title: 파스칼의 삼각형, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240702/파스칼의 삼각형/pascal's-triangle.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+arr = [
+    [0 for _ in range(n)]
+        for _ in range(n)
+]
+
+arr[0][0] = 1
+
+for i in range(1, n):
+    for j in range(i + 1):
+        arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+for i in range(n):
+    for j in range(i+1):
+        print(arr[i][j], end=' ')
+        if(j == i):
+            break
+    print()
