36987eb0de105f872422c1677f4c01d3e3ba0cf1
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-03 12:11:56 +0900

    Title: Run Length 인코딩, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240703/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,20 @@
+s = input()
+aph = s[0]
+cnt = 0
+enc = []
+for i in range(len(s)):
+    if(aph != s[i]):
+        enc.append(aph)
+        enc.append(cnt)
+        aph = s[i]
+        cnt = 0
+    if(aph == s[i]):
+        cnt += 1
+    if(i == len(s) - 1):
+        enc.append(aph)
+        enc.append(cnt)
+
+print(len(enc))
+
+for i in range(len(enc)):
+    print(enc[i],end='')
