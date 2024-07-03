62b2fb5296f0fd2d63f02854615bfd04cb0c1f93
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-03 19:27:11 +0900

    Title: Run Length 인코딩, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240703/Run Length 인코딩/run-length-encoding.py
+++ 240703/Run Length 인코딩/run-length-encoding.py
@@ -3,14 +3,29 @@ aph = s[0]
 cnt = 0
 enc = []
 for i in range(len(s)):
-    if(aph != s[i]):
-        enc.append(aph)
-        cnt = str(cnt)
-        enc.append(cnt)
-        aph = s[i]
-        cnt = 0
+
     if(aph == s[i]):
         cnt += 1
+
+    if(aph != s[i]):
+        enc.append(aph)
+
+        if(cnt >= 10):
+            digit = []
+            while(cnt >= 10):
+                digit.append(cnt % 10)
+                cnt //= 10
+            digit.append(cnt)
+
+            for j in range(len(digit) - 1, -1, -1):
+                enc += str(digit[j])
+
+        else:
+            enc.append(cnt)
+        
+        aph = s[i]
+        cnt = 1
+
     if(i == len(s) - 1):
         enc.append(aph)
         enc.append(cnt)
