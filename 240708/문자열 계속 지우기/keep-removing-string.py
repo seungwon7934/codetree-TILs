a4ba41b4d4b5eec1bc4a72930c82d9e30a49a73a
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-08 14:37:58 +0900

    Title: 문자열 계속 지우기, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/문자열 계속 지우기/keep-removing-string.py
+++ 240708/문자열 계속 지우기/keep-removing-string.py
@@ -6,9 +6,9 @@ isTrue = False
 while(isTrue != True):
     i = 0
     for i in range(len(a) - 1):
-        if(a[i:i+2] == b):
-            a.pop(i)
-            a.pop(i)
+        if(a[i:i+len(b)] == b):
+            for j in range(len(b)):
+                a.pop(i)
             break
     
     if(''.join(a).find(''.join(b)) == -1):
