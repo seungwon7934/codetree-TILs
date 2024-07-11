2185b32df8c649a41b2c077197e042db441a5fdf
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-11 11:13:16 +0900

    Title: 문자열의 개수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240711/문자열의 개수/number-of-spring.py
@@ -0,0 +1,19 @@
+arr = []
+cnt = 0
+
+for _ in range(200):
+    s = input()
+    if(s == '0'):
+        break
+    else:
+        arr.append(s)
+        cnt += 1
+
+print(cnt)
+
+if(len(arr) % 2 == 0):
+    for i in range(1, len(arr), 2):
+        print(arr[i])
+else:
+    for i in range(0, len(arr), 2):
+        print(arr[i])
