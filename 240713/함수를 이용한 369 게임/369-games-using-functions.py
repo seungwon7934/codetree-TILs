e73692eda47ec0d6cf964475e479e8e85dab6ecf
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-13 12:52:57 +0900

    Title: 함수를 이용한 369 게임, Time: 170ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240713/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,24 @@
+def find_digits(n):
+    isTrue = False
+    
+    while(n != 0):
+        q = n // 10
+        r = n % 10
+
+        if((q != 0 and r != 0) and (q % 3 == 0 or r % 3 == 0)):
+            isTrue = True
+            break
+        n = q
+    return isTrue
+
+def three_multiple(n):
+    return n % 3 == 0
+
+a, b = map(int, input().split())
+total = 0
+
+for i in range(a, b + 1):
+    if(find_digits(i) or three_multiple(i)):
+        total += 1
+
+print(total)
