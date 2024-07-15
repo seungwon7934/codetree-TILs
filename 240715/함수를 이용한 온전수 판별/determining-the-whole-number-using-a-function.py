0ccbe0bc2db250606ecaf818ba76fbb702461fd4
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-15 13:27:04 +0900

    Title: 함수를 이용한 온전수 판별, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,17 @@
+def isPerfect(n):
+    if(n % 2 == 0):
+        return False
+    if(n % 10 == 5):
+        return False
+    if(n % 3 == 0 and n % 9 != 0):
+        return False
+    return True
+
+a, b = map(int, input().split())
+total = 0
+
+for i in range(a, b):
+    if(isPerfect(i)):
+        total += 1
+
+print(total)
