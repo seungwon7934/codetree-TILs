562cd23560fb31d861ae8be925594774d76d95e5
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-07-18 09:44:41 +0900

    Title: 2개 이상의 알파벳, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240718/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240718/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,6 +1,6 @@
 def dup(s):
     for i in range(len(s)):
-        for j in range(i+1, len(s) - 1):
+        for j in range(i+1, len(s)):
             if(s[i] != s[j]):
                 return "Yes"
     return "No"
