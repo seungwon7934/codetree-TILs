a6a5ba8307ee00a2fc1cd2b30cdc45e15b18a2ef
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-09-19 16:01:25 +0900

    Title: 국영수 순이지, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240919/국영수 순이지/korean-english-math-order.py
+++ 240919/국영수 순이지/korean-english-math-order.py
@@ -13,7 +13,10 @@ for _ in range(n):
     arr = input().split()
     s.append(Score(arr[0], arr[1], arr[2], arr[3]))
 
-s.sort(key=lambda x:(-int(x.kor), -int(x.eng), -int(x.math)))
+# s.sort(key=lambda x:(-int(x.kor), -int(x.eng), -int(x.math)))
+s.sort(key=lambda x:(x.kor, x.eng, x.math))
 
-for ss in s:
-    print(ss.name, ss.kor, ss.eng, ss.math)
+
+for i in range(n-1, -1, -1):
+    pp = s[i]
+    print(pp.name, pp.kor, pp.eng, pp.math)
