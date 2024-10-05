8bd7b692d0be3693f8362494fa2eabaaec8f6cd9
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-05 17:06:22 +0900

    Title: 진수 to 진수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/진수 to 진수/transformation-of-number-system.py
+++ 241005/진수 to 진수/transformation-of-number-system.py
@@ -1,22 +1,37 @@
+def change(n, num):
+    l = []
+    while(n != 0):
+        l.append(n % num)
+        n //= num
+    l.reverse()
+    return l
+
 a, b = map(int, input().split())
 n = int(input())
 l = []
 total = 0
 
-while(n != 0):
-    l.append(n % 10)
-    n //= 10
-l.reverse()
+l = change(n, 10)
+# print(l)
+
+
+
+# while(n != 0):
+#     l.append(n % 10)
+#     n //= 10
+# l.reverse()
 
 for m in l:
     total = a * total + int(m)
 
 l.clear()
 
-while(total != 0):
-    l.append(total % b)
-    total //= b
-l.reverse()
+l = change(total, b)
+
+# while(total != 0):
+#     l.append(total % b)
+#     total //= b
+# l.reverse()
 
 for m in l:
     print(m,end='')
