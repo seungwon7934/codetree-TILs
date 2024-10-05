106b5907fcb26b0fa7d5ef6ff79f4472b1e2e85e
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-05 17:14:52 +0900

    Title: 블럭쌓는 명령2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241005/블럭쌓는 명령2/block-stacking-commands2.py
@@ -4,10 +4,10 @@ l = [0] * (n + 1)
 
 for _ in range(k):
     a, b = map(int, input().split())
-    for i in range(a, b+1):
+    for i in range(a, b + 1):
         l[i] += 1
 m = 0
-for _ in range(len(l)):
+for i in range(len(l)):
     if (m < l[i]):
         m = l[i]
 
