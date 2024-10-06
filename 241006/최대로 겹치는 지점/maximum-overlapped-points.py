c0722ed72730889c407b3602119e15bfe4675a4c
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-06 11:13:56 +0900

    Title: 최대로 겹치는 지점, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241006/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+line = [0] * 201
+
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    x1 += 100
+    x2 += 100
+
+    for i in range(x1, x2+1):
+        line[i] += 1
+
+m = 0
+
+for num in line:
+    if(m < num):
+        m = num
+
+print(m)
