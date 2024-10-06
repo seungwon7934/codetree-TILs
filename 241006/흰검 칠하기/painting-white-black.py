3724b44b912607d6b5edf0aabc140d4b68727aea
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-06 12:35:55 +0900

    Title: 흰검 칠하기, Time: 212ms, Memory: 33MB, Status: Passed - CodeTree

--- 241006/흰검 칠하기/painting-white-black.py
+++ 241006/흰검 칠하기/painting-white-black.py
@@ -4,107 +4,164 @@ class Tile:
         self.color = color
         self.count = count
 
-
 n = int(input())
 idx = 1000000
-line = []
+line = {}  # 딕셔너리로 변경
+
 for _ in range(n):
     x, d = input().split()
     x = int(x)
 
-    if(d == "L"):
-        if(len(line) == 0):
-            for i in range(idx, idx - x, -1):
-                line.append(Tile(i, "w", 1))
-
-        else:
-            for i in range(idx, idx - x, -1):
-                exist = False
-
-                for tile in line:
-                    if(tile.index == i):
-                        tile.color += "w"
-                        tile.count += 1
-                        exist = True
-                        break;
-
-                if(exist == False):
-                    line.append(Tile(i, "w", 1))
+    if d == "L":
+        for i in range(idx, idx - x, -1):
+            if i in line:
+                line[i].color += "w"
+                line[i].count += 1
+            else:
+                line[i] = Tile(i, "w", 1)
         idx -= x - 1
-
     else:
-        if(len(line) == 0):
-            for i in range(idx, idx + x):
-                line.append(Tile(i, "b", 1))
-
-        else:
-            for i in range(idx, idx + x):
-                exist = False
-
-                for tile in line:
-                    if(tile.index == i):
-                        tile.color += "b"
-                        tile.count += 1
-                        exist = True
-                        break;
-                        
-                if(exist == False):
-                    line.append(Tile(i, "b", 1))
+        for i in range(idx, idx + x):
+            if i in line:
+                line[i].color += "b"
+                line[i].count += 1
+            else:
+                line[i] = Tile(i, "b", 1)
         idx += x - 1
 
 w = b = g = 0
 
-# for tile in line:
-#     print(tile.index, tile.color, tile.count)
-
-# for tile in line:
-#     arr = list(tile.color)
-
-#     count_w = 0
-#     count_b = 0
-
-#     for c in arr:
-#         if(c == "w"):
-#             count_w += 1
-#         else:
-#             count_b += 1
-
-#     if(count_w >= 2 and count_b >= 2):
-#         g += 1
-
-#     else:
-#         if(arr[len(arr) - 1] == "w"):
-#             w += 1
-#         else:
-#             b += 1
-
-# print(w, b, g)
-
-for tile in line:
+for tile in line.values():  # 딕셔너리에서 값들만 순회
     arr = list(tile.color)
 
-    if(len(arr) < 4):
-        if(arr[len(arr)-1] == "w"):
+    if len(arr) < 4:
+        if arr[-1] == "w":
             w += 1
         else:
             b += 1
     else:
-        count_w = 0
-        count_b = 0
+        count_w = arr.count("w")
+        count_b = arr.count("b")
 
-        for c in arr:
-            if(c == "w"):
-                count_w += 1
-            else:
-                count_b += 1
-
-        if(count_w >= 2 and count_b >= 2):
+        if count_w >= 2 and count_b >= 2:
             g += 1
-
         else:
-            if(arr[len(arr) - 1] == "w"):
+            if arr[-1] == "w":
                 w += 1
             else:
                 b += 1
 
 print(w, b, g)
+
+
+
+# class Tile:
+#     def __init__(self, index=0, color='none', count=0):
+#         self.index = index
+#         self.color = color
+#         self.count = count
+
+
+# n = int(input())
+# idx = 1000000
+# line = []
+# for _ in range(n):
+#     x, d = input().split()
+#     x = int(x)
+
+#     if(d == "L"):
+#         if(len(line) == 0):
+#             for i in range(idx, idx - x, -1):
+#                 line.append(Tile(i, "w", 1))
+
+#         else:
+#             for i in range(idx, idx - x, -1):
+#                 exist = False
+
+#                 for tile in line:
+#                     if(tile.index == i):
+#                         tile.color += "w"
+#                         tile.count += 1
+#                         exist = True
+#                         break
+
+#                 if(exist == False):
+#                     line.append(Tile(i, "w", 1))
+#         idx -= x - 1
+
+#     else:
+#         if(len(line) == 0):
+#             for i in range(idx, idx + x):
+#                 line.append(Tile(i, "b", 1))
+
+#         else:
+#             for i in range(idx, idx + x):
+#                 exist = False
+
+#                 for tile in line:
+#                     if(tile.index == i):
+#                         tile.color += "b"
+#                         tile.count += 1
+#                         exist = True
+#                         break
+                        
+#                 if(exist == False):
+#                     line.append(Tile(i, "b", 1))
+#         idx += x - 1
+
+# w = b = g = 0
+
+# # for tile in line:
+# #     print(tile.index, tile.color, tile.count)
+
+# # for tile in line:
+# #     arr = list(tile.color)
+
+# #     count_w = 0
+# #     count_b = 0
+
+# #     for c in arr:
+# #         if(c == "w"):
+# #             count_w += 1
+# #         else:
+# #             count_b += 1
+
+# #     if(count_w >= 2 and count_b >= 2):
+# #         g += 1
+
+# #     else:
+# #         if(arr[len(arr) - 1] == "w"):
+# #             w += 1
+# #         else:
+# #             b += 1
+
+# # print(w, b, g)
+
+# for tile in line:
+#     arr = list(tile.color)
+
+#     if(len(arr) < 4):
+#         if(arr[len(arr)-1] == "w"):
+#             w += 1
+#         else:
+#             b += 1
+#     else:
+#         count_w = 0
+#         count_b = 0
+
+#         for c in arr:
+#             if(c == "w"):
+#                 count_w += 1
+#             else:
+#                 count_b += 1
+
+#         if(count_w >= 2 and count_b >= 2):
+#             g += 1
+
+#         else:
+#             if(arr[len(arr) - 1] == "w"):
+#                 w += 1
+#             else:
+#                 b += 1
+
+# print(w, b, g)
