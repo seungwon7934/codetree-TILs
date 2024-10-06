dc0f258c5bdb481236632a5f81aec97a1d24beda
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-06 12:00:16 +0900

    Title: 흰검 칠하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/흰검 칠하기/painting-white-black.py
+++ 241006/흰검 칠하기/painting-white-black.py
@@ -1,9 +1,64 @@
+class Tile:
+    def __init__(self, index=0, color='none', count=0):
+        self.index = index
+        self.color = color
+        self.count = count
+
+
 n = int(input())
-arr = [[0 for _ in range(3)] for _ in range(3)]
-line = [[0 for _ in range(2) for _ in range(2000001)]]
 idx = 1000000
+line = []
 for _ in range(n):
     x, d = input().split()
+    x = int(x)
 
-    
-print(line)
+    if(d == "L"):
+        if(len(line) == 0):
+            for i in range(idx - x, idx):
+                line.append(Tile(i, "w", 1))
+
+        else:
+            for i in range(idx - x, idx):
+                exist = False
+
+                for tile in line:
+                    if(tile.index == i):
+                        tile.color = "w"
+                        tile.count += 1
+                        exist = True
+
+                if(exist == False):
+                    line.append(Tile(i, "w", 1))
+        idx -= x
+
+    else:
+        if(len(line) == 0):
+            for i in range(idx, idx + x):
+                line.append(Tile(i, "b", 1))
+
+        else:
+            for i in range(idx, idx + x):
+                exist = False
+
+                for tile in line:
+                    if(tile.index == i):
+                        tile.color = "b"
+                        tile.count += 1
+                        exist = True
+                        
+                if(exist == False):
+                    line.append(Tile(i, "b", 1))
+        idx += x
+
+w = b = g = 0
+
+for tile in line:
+    if(tile.count >= 4):
+        g += 1
+    else:
+        if(tile.color == "w"):
+            w += 1
+        else:
+            b += 1
+
+print(w, b, g)
