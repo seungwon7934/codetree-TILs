f016f9cc0ef036b6678da5a7dddfe4e158c471af
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-11 14:33:10 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 241011/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,7 +1,5 @@
-c = {}
-
-min_x = 2000
-min_y = 2000
+c = []
+area = 0
 
 for i in range(3):
 
@@ -12,20 +10,18 @@ for i in range(3):
     x2 += 1000
     y2 += 1000
 
-    if(min_x > x1):
-        min_x = x1
-    
-    if(min_y > y1):
-        min_y = y1
 
-    if(i < 2):
-        for x in range(x1, x2):
-            for y in range(y1, y2):
-                c[(x,y)] = 1
+    if(i < 2):  # 겹치지 않는 직사각형 A, B의 면적 계산 후 저장, 좌표 저장
+        area += (x2 - x1) * (y2 - y1)
+        c.append( (x1, y1, x2, y2) )
     else:
-        for x in range(x1, x2):
-            for y in range(y1, y2):
-                if( (x,y) in c):
-                    del c[(x, y)]
+        for j in range(2):  # 빼줘야 하는 직사각형의 좌표에 대해 A, B에 대해 겹치는 좌표 경계를 계산
+            overlap_x1 = max(x1, c[j][0])
+            overlap_y1 = max(y1, c[j][1])   # 더 오른쪽 값 선택 (바깥쪽)
+            overlap_x2 = min(x2, c[j][2])
+            overlap_y2 = min(y2, c[j][3])   # 더 왼쪽 값 선택 (안쪽)
 
-print(len(c.keys()))
+            if(overlap_x1 < overlap_x2 and overlap_y1 < overlap_y2):
+                area -= (overlap_x2 - overlap_x1) * (overlap_y2 - overlap_y1)
+
+print(area)
