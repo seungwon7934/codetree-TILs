081bfebcdeb11ee681d0827e50f0afa07b5beb8e
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-16 12:43:09 +0900

    Title: 연속되는 수 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/연속되는 수 2/continuous-number2.py
+++ 241016/연속되는 수 2/continuous-number2.py
@@ -4,12 +4,19 @@ n = int(input())
 cnt = 0
 for i in range(n):
     a.append(int(input()))
-    if(i == 0 or a[i] == a[i-1]):
+
+    if(i == 0):
         cnt += 1
     
-        if(cnt > m):
-            m = cnt + 1
+    elif(cnt == 0 and a[i] != a[i-1]):
+        cnt += 1
+
+    elif(a[i] == a[i-1]):
+        cnt += 1
+
     else:
-        cnt = 0
+        if(cnt > m):
+            m = cnt
+        cnt = 1
 
 print(m)
