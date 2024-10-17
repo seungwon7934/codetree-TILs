23bfd9fa6098c94455bfbe51e5225134d3488dcc
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-17 16:00:26 +0900

    Title: 좌우로 움직이는 로봇, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,50 @@
+def moving(cnt, arr):
+    idx = 0
+    time = 0
+    for _ in range(cnt):
+        t, d = input().split()
+        t = int(t)
+
+        if(d == "R"):
+            for i in range(time, time + t):
+                idx += 1
+                arr[i] = idx
+        else:
+            for i in range(time, time + t):
+                idx -= 1
+                arr[i] = idx
+        time += t
+
+
+n, m = map(int, input().split())
+a = {}
+b = {}
+l = 0
+t = 0
+cnt = 0
+moving(n, a)
+len_a = len(a)
+
+moving(m, b)
+len_b = len(b)
+
+if(len_a >= len_b): # b가 더 짧을 때
+    t = len_b
+    for i in range(1, t):
+        if(a[i-1] != b[i-1] and a[i] == b[i]):
+            cnt += 1
+    
+    for i in range(t, len_a):
+        if(b[t-2] != a[i-1] and b[t-1] == b[i]):
+            cnt += 1
+else:   # a가 더 짧을 때
+    t = len_a
+    for i in range(1, t):
+        if(a[i-1] != b[i-1] and a[i] == b[i]):
+            cnt += 1
+    
+    for i in range(t, len_b):
+        if(a[t-2] != b[i-1] and a[t-1] == b[i]):
+            cnt += 1
+
+print(cnt)
