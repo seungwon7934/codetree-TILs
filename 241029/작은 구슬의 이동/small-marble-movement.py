759d257aa55a20aa2bfea20392255dfe15cc7e24
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-29 16:11:02 +0900

    Title: 작은 구슬의 이동, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 241029/작은 구슬의 이동/small-marble-movement.py
+++ 241029/작은 구슬의 이동/small-marble-movement.py
@@ -1,15 +1,18 @@
 dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
 dir_num = 0
+
 n, t = map(int, input().split())
 r, c, d = input().split()
+
 r = int(r)
 c = int(c)
+
 y, x = r-1, c-1
 
 if(d == "U"):
-    dir_num = 0
-elif(d == "D"):
     dir_num = 1
+elif(d == "D"):
+    dir_num = 0
 elif(d == "L"):
     dir_num = 2
 else:
@@ -28,11 +31,11 @@ for _ in range(t):
         x += 1
         continue
     if(y == n):
-        dir_num = 0
+        dir_num = 1
         y -= 1
 
     if(y == -1):
-        dir_num = 1
+        dir_num = 0
         y += 1
 
 print(y+1, x+1)
