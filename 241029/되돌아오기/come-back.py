93abb3eb15466d8ee1e4cd670386bbb610761235
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2024-10-29 18:07:47 +0900

    Title: 되돌아오기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241029/되돌아오기/come-back.py
+++ 241029/되돌아오기/come-back.py
@@ -9,14 +9,9 @@ def moving(dir_num, arr, m):
     global x, y, dx, dy, t
     for i in range(t, t + m):
         x, y = x + dx[dir_num], y + dy[dir_num]
-
-        if(x == 0 and y == 0):
-            return i
-
         arr[i] = (y, x)
     
     t += m
-    return 0
 
 
 result = 0
@@ -25,18 +20,17 @@ for i in range(n):
     d, m = input().split()
     m = int(m)
     if(d == "N"):
-        idx = moving(0, arr, m)
+        moving(0, arr, m)
     elif(d == "S"):
-        idx = moving(1, arr, m)
+        moving(1, arr, m)
     elif(d == "W"):
-        idx = moving(2, arr, m)
+        moving(2, arr, m)
     else:
-        idx = moving(3, arr, m)
+        moving(3, arr, m)
 
-    if(idx != 0):
-        result = idx
-
-if(result == 0):
-    print(-1)
-else:
-    print(result)
+for i in arr:
+    if(arr[i] == (0, 0)):
+        print(i)
+        break
+    if(i == len(arr) - 1 and arr[i] != (0, 0)):
+        print(-1)
